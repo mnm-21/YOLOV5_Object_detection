c55caaea0cc69951be47fba888007bfe35dac2a7
Mayank Chandak <127128604+mnm-21@users.noreply.github.com>
2024-01-11 03:47:32 +0530

    Update web_deployment_yolo.py

--- web_deployment_yolo.py
+++ web_deployment_yolo.py
@@ -76,7 +76,7 @@ def predictions(image):
         text = f'{class_name.upper()}: {bb_conf}'
         cv2.rectangle(image, (x, y), (x + w, y + h), colours, 2)
         cv2.rectangle(image, (x, y - 30), (x + w, y), colours, -1)
-        cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1.2, (0, 0, 0), 1)
+        cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 0.7, (0, 0, 0), 1)
 
     return image
 
